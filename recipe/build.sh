f99d748bd2a12e15e62e817729e6ee1f3581c51f
Lars Bilke <lars.bilke@ufz.de>
2026-08-27 12:03:17 +0200

    Fix macOS build with clang 21

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,9 @@ set -ex
 
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    # clang 21 no longer exposes these declarations transitively through the
+    # macOS SDK headers used by OGS's vendored spdlog/fmt.
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -include stdlib.h"
 fi
 
 ENABLE_MFRONT="OFF"
